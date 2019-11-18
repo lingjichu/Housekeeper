50b750e45e90259e26eb4f6193d967c9ee7f3a45
liurunqiu <928333210@qq.com>
2019-11-18 15:33:35 +0800

    a

---
+++ MainApp.java
@@ -0,0 +1,10 @@
+package cn.sjzc.gjp.app;
+
+import cn.sjzc.gjp.controller.MainFrameController;
+
+public class MainApp extends MainFrameController {
+     public static void main(String[] args) {
+		  new MainFrameController().setVisible(true);
+	}
+
+}
